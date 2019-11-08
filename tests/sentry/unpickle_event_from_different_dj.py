75c5f86c1b06102e5d9ab120a0ad92412cf3b521
Joshua Li <josh@jrl.ninja>
2019-11-08 10:35:41 -0800

    stricter comparison with model dicts

--- tests/sentry/unpickle_event_from_different_dj.py
+++ tests/sentry/unpickle_event_from_different_dj.py
@@ -1,6 +1,7 @@
 import pickle
 
 import django
+from django.forms.models import model_to_dict
 
 from sentry.testutils import TestCase
 from sentry.testutils.helpers.datetime import iso_format, before_now
@@ -12,6 +13,7 @@ class UnpickleEventFromDifferentDjango(TestCase):
         event = self.store_event(
             data={
                 "event_id": "a" * 32,
+                # XXX: store_event doesn't really like datetime.fromtimestamp(0) or similar
                 "timestamp": iso_format(before_now(minutes=1)),
                 "fingerprint": ["foobar"],
             },
@@ -27,4 +29,14 @@ class UnpickleEventFromDifferentDjango(TestCase):
         with open("event-" + target_dj_version[".".join(map(str, django.VERSION[:2]))] + ".pickle", "rb") as f:
             event_from_target_dj = pickle.load(f)
 
-        assert event == event_from_target_dj
+        event_data = model_to_dict(event)
+        event_from_target_dj_data = model_to_dict(event_from_target_dj)
+
+        event_data.pop("datetime")
+        event_from_target_dj_data.pop("datetime")
+        event_data["data"].pop("received")
+        event_from_target_dj_data["data"].pop("received")
+        event_data["data"].pop("timestamp")
+        event_from_target_dj_data["data"].pop("timestamp")
+
+        assert event_data == event_from_target_dj_data
