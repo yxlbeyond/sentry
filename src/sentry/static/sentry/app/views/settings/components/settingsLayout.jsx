b124417363119a56f2631eada7bdcc0913225ec1
jmanhart <jmanhart@sentry.io>
2019-12-06 17:23:49 -0600

    PR updates/edit to the settings UI on smaller browser widths

--- src/sentry/static/sentry/app/views/settings/components/settingsLayout.jsx
+++ src/sentry/static/sentry/app/views/settings/components/settingsLayout.jsx
@@ -14,9 +14,11 @@ class SettingsLayout extends React.Component {
     router: PropTypes.object,
     routes: PropTypes.array,
   };
+
   state = {
     navVisible: true,
   };
+
   render() {
     const {params, routes, route, router, renderNavigation, children} = this.props;
     // We want child's view's props
@@ -29,6 +31,7 @@ class SettingsLayout extends React.Component {
           <SettingsHeader>
             <StyledBreadcrumbWrapper>
               <SettingsHamburgerIcon
+                priority="link"
                 icon={this.state.navVisible ? 'icon-activity' : 'icon-close'}
                 onClick={() => this.setState({navVisible: !this.state.navVisible})}
               />
@@ -46,6 +49,7 @@ class SettingsLayout extends React.Component {
                 {renderNavigation()}
               </SidebarWrapper>
             )}
+            <Mask isVisible={this.state.navVisible} />
             <Content>{children}</Content>
           </MaxWidthContainer>
         </SettingsColumn>
@@ -54,22 +58,43 @@ class SettingsLayout extends React.Component {
   }
 }
 
+const Mask = styled('div')`
+  @media (max-width: ${p => p.theme.breakpoints[0]}) {
+    display: ${p => (p.isVisible ? 'none' : 'block')};
+    background: rgba(0, 0, 0, 0.35);
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    z-index: 2;
+    animation: 0.25s ease-in-out comeIn;
+    @keyframes comeIn {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+  }
+`;
 const StyledBreadcrumbWrapper = styled('div')`
   display: flex;
 `;
 
 const SettingsHamburgerIcon = styled(Button)`
-  margin-right: 16px;
+  margin-right: ${space(2)};
   @media (min-width: ${p => p.theme.breakpoints[0]}) {
     display: ${p => (p.isVisible ? 'block' : 'none')};
   }
 `;
+
 const MaxWidthContainer = styled('div')`
   display: flex;
   max-width: ${p => p.theme.settings.containerWidth};
   min-width: 600px; /* for small screen sizes, we need a min width to make it semi digestible */
   flex: 1;
 `;
+
 const SidebarWrapper = styled('div')`
   flex-shrink: 0;
   width: ${p => p.theme.settings.sidebarWidth};
@@ -78,10 +103,30 @@ const SidebarWrapper = styled('div')`
   padding: ${space(4)};
   @media (max-width: ${p => p.theme.breakpoints[0]}) {
     display: ${p => (p.isVisible ? 'none' : 'block')};
-    position: sticky;
-    z-index: 1;
+    animation: ${p => (p.isVisible ? 'menuOut' : 'menuIn')} 0.35s ease-in-out;
+    position: absolute;
+    z-index: 3;
+    height: 100%;
+    box-shadow: 0 6px 15px rgba(0, 0, 0, 0.15);
+    @keyframes menuIn {
+      from {
+        left: -${p => p.theme.settings.sidebarWidth};
+      }
+      to {
+        left: 0;
+      }
+    }
+    @keyframes menuOut {
+      from {
+        left: 0;
+      }
+      to {
+        left: ${p => p.theme.settings.sidebarWidth};
+      }
+    }
   }
 `;
+
 const SettingsColumn = styled('div')`
   display: flex;
   flex-direction: column;
