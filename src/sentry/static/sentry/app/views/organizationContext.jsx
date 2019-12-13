bac6d9e015a161099b0415280b52663d76c1a7ba
davidw1339 <davidw1339@gmail.com>
2019-12-13 11:51:49 -0800

    address comments

--- src/sentry/static/sentry/app/views/organizationContext.jsx
+++ src/sentry/static/sentry/app/views/organizationContext.jsx
@@ -158,7 +158,8 @@ const OrganizationContext = createReactClass({
     return (
       loading ||
       (!error &&
-        (detailed && (!organization || !organization.projects || !organization.teams)))
+        detailed &&
+        (!organization || !organization.projects || !organization.teams))
     );
   },
 
@@ -200,7 +201,9 @@ const OrganizationContext = createReactClass({
           ({path}) => path && path.includes('/organizations/:orgId/issues/:groupId/')
         )
       ) {
-        GlobalSelectionStore.loadInitialData(organization, this.props.location.query);
+        GlobalSelectionStore.loadInitialData(organization, this.props.location.query, {
+          api: this.props.api,
+        });
       }
     } else if (error) {
       // If user is superuser, open sudo window
