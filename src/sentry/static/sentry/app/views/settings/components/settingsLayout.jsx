08bde297315317af8f5e81f3cf2611e7e08187a5
jmanhart <jmanhart@sentry.io>
2019-12-16 16:30:44 -0600

    UI updates to make the settings UI work on smaller browsers and mobile

--- src/sentry/static/sentry/app/views/settings/components/settingsLayout.jsx
+++ src/sentry/static/sentry/app/views/settings/components/settingsLayout.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'react-emotion';
 import space from 'app/styles/space';
+import {slideInLeft, fadeIn} from 'app/styles/animations';
 import Button from 'app/components/button';
 import SettingsBreadcrumb from './settingsBreadcrumb';
 import SettingsHeader from './settingsHeader';
@@ -16,8 +17,26 @@ class SettingsLayout extends React.Component {
   };
 
   state = {
-    navVisible: true,
+    navVisible: false,
   };
+
+  componentWillMount() {
+    this.unlisten = this.props.router.listen(() => {
+      this.toggleNav(false);
+    });
+  }
+  componentWillUnmount() {
+    this.unlisten();
+  }
+
+  toggleNav(navVisible) {
+    this.setState({navVisible});
+    if (navVisible) {
+      document.getElementsByTagName('body')[0].classList.add('modal-open');
+    } else {
+      document.getElementsByTagName('body')[0].classList.remove('modal-open');
+    }
+  }
 
   render() {
     const {params, routes, route, router, renderNavigation, children} = this.props;
@@ -25,6 +44,7 @@ class SettingsLayout extends React.Component {
     const childProps = (children && children.props) || this.props;
     const childRoutes = childProps.routes || routes || [];
     const childRoute = childProps.route || route || {};
+
     return (
       <React.Fragment>
         <SettingsColumn>
@@ -32,8 +52,9 @@ class SettingsLayout extends React.Component {
             <StyledBreadcrumbWrapper>
               <SettingsHamburgerIcon
                 priority="link"
-                icon={this.state.navVisible ? 'icon-activity' : 'icon-close'}
-                onClick={() => this.setState({navVisible: !this.state.navVisible})}
+                iconSize="16px"
+                icon={this.state.navVisible ? 'icon-close' : 'icon-activity'}
+                onClick={() => this.toggleNav(!this.state.navVisible)}
               />
               <SettingsBreadcrumb
                 params={params}
@@ -49,7 +70,7 @@ class SettingsLayout extends React.Component {
                 {renderNavigation()}
               </SidebarWrapper>
             )}
-            <Mask isVisible={this.state.navVisible} />
+            <NavMask isVisible={this.state.navVisible} />
             <Content>{children}</Content>
           </MaxWidthContainer>
         </SettingsColumn>
@@ -58,33 +79,28 @@ class SettingsLayout extends React.Component {
   }
 }
 
-const Mask = styled('div')`
+const NavMask = styled('div')`
   @media (max-width: ${p => p.theme.breakpoints[0]}) {
-    display: ${p => (p.isVisible ? 'none' : 'block')};
+    display: ${p => (p.isVisible ? 'block' : 'none')};
     background: rgba(0, 0, 0, 0.35);
     height: 100%;
     width: 100%;
     position: absolute;
     z-index: 2;
-    animation: 0.25s ease-in-out comeIn;
-    @keyframes comeIn {
-      from {
-        opacity: 0;
-      }
-      to {
-        opacity: 1;
-      }
-    }
+    animation: ${fadeIn} 250ms ease-in-out;
   }
 `;
+
 const StyledBreadcrumbWrapper = styled('div')`
   display: flex;
 `;
 
 const SettingsHamburgerIcon = styled(Button)`
   margin-right: ${space(2)};
-  @media (min-width: ${p => p.theme.breakpoints[0]}) {
-    display: ${p => (p.isVisible ? 'block' : 'none')};
+  color: ${p => p.theme.gray5};
+  display: none;
+  @media (max-width: ${p => p.theme.breakpoints[0]}) {
+    display: ${p => (p.isVisible ? 'none' : 'block')};
   }
 `;
 
@@ -96,34 +112,17 @@ const MaxWidthContainer = styled('div')`
 `;
 
 const SidebarWrapper = styled('div')`
-  flex-shrink: 0;
   width: ${p => p.theme.settings.sidebarWidth};
   background: #fff;
   border-right: 1px solid ${p => p.theme.borderLight};
   padding: ${space(4)};
   @media (max-width: ${p => p.theme.breakpoints[0]}) {
-    display: ${p => (p.isVisible ? 'none' : 'block')};
-    animation: ${p => (p.isVisible ? 'menuOut' : 'menuIn')} 0.35s ease-in-out;
+    animation: ${slideInLeft} 100ms ease-in-out;
+    display: ${p => (p.isVisible ? 'block' : 'none')};
     position: absolute;
     z-index: 3;
     height: 100%;
     box-shadow: 0 6px 15px rgba(0, 0, 0, 0.15);
-    @keyframes menuIn {
-      from {
-        left: -${p => p.theme.settings.sidebarWidth};
-      }
-      to {
-        left: 0;
-      }
-    }
-    @keyframes menuOut {
-      from {
-        left: 0;
-      }
-      to {
-        left: ${p => p.theme.settings.sidebarWidth};
-      }
-    }
   }
 `;
 
@@ -136,13 +135,16 @@ const SettingsColumn = styled('div')`
     margin-top: 0;
   }
 `;
+
 /**
  * Note: `overflow: hidden` will cause some buttons in `SettingsPageHeader` to be cut off because it has negative margin.
  * Will also cut off tooltips.
  */
+
 const Content = styled('div')`
   flex: 1;
   padding: ${space(4)};
   min-width: 0; /* keep children from stretching container */
 `;
+
 export default SettingsLayout;
