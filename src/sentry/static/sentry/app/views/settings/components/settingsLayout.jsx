15ca718a77ee45e620cf8b19b41649bbba682f42
Evan Purkhiser <evanpurkhiser@gmail.com>
2019-12-18 12:34:46 -0800

    Tweaks
     - Added some comments on what the navVisible state is for
     - Use componentDidMount over componentWillMount
     - Minor formatting changes / css property ordering (just personal
       preference)
     - Cleanup toggleNav by onelining the class add/remove
     - Fix tests by adding the router params where needed

--- src/sentry/static/sentry/app/views/settings/components/settingsLayout.jsx
+++ src/sentry/static/sentry/app/views/settings/components/settingsLayout.jsx
@@ -17,29 +17,33 @@ class SettingsLayout extends React.Component {
   };
 
   state = {
+    /**
+     * This is used when the screen is small enough that the navigation should
+     * be hidden. On large screens this state will end up unused.
+     */
     navVisible: false,
   };
 
-  componentWillMount() {
-    this.unlisten = this.props.router.listen(() => {
-      this.toggleNav(false);
-    });
+  componentDidMount() {
+    // Close the navigation when navigating. Only on small screens.
+    this.unlisten = this.props.router.listen(() => this.toggleNav(false));
   }
+
   componentWillUnmount() {
     this.unlisten();
   }
 
   toggleNav(navVisible) {
     this.setState({navVisible});
-    if (navVisible) {
-      document.getElementsByTagName('body')[0].classList.add('modal-open');
-    } else {
-      document.getElementsByTagName('body')[0].classList.remove('modal-open');
-    }
+    const bodyElement = document.getElementsByTagName('body')[0];
+
+    // XXX(epurkhiser): The 'modal-open' class scroll-locks the body
+    bodyElement.classList[navVisible ? 'add' : 'remove']('modal-open');
   }
 
   render() {
     const {params, routes, route, router, renderNavigation, children} = this.props;
+    const {navVisible} = this.state;
     // We want child's view's props
     const childProps = (children && children.props) || this.props;
     const childRoutes = childProps.routes || routes || [];
@@ -49,28 +53,26 @@ class SettingsLayout extends React.Component {
       <React.Fragment>
         <SettingsColumn>
           <SettingsHeader>
-            <StyledBreadcrumbWrapper>
-              <SettingsHamburgerIcon
+            <BreadcrumbWrapper>
+              <NavMenuToggle
                 priority="link"
                 iconSize="16px"
-                icon={this.state.navVisible ? 'icon-close' : 'icon-activity'}
-                onClick={() => this.toggleNav(!this.state.navVisible)}
+                icon={navVisible ? 'icon-close' : 'icon-activity'}
+                onClick={() => this.toggleNav(!navVisible)}
               />
               <SettingsBreadcrumb
                 params={params}
                 routes={childRoutes}
                 route={childRoute}
               />
-            </StyledBreadcrumbWrapper>
+            </BreadcrumbWrapper>
             <SettingsSearch routes={routes} router={router} params={params} />
           </SettingsHeader>
           <MaxWidthContainer>
             {typeof renderNavigation === 'function' && (
-              <SidebarWrapper isVisible={this.state.navVisible}>
-                {renderNavigation()}
-              </SidebarWrapper>
+              <SidebarWrapper isVisible={navVisible}>{renderNavigation()}</SidebarWrapper>
             )}
-            <NavMask isVisible={this.state.navVisible} />
+            <NavMask isVisible={navVisible} />
             <Content>{children}</Content>
           </MaxWidthContainer>
         </SettingsColumn>
@@ -79,7 +81,13 @@ class SettingsLayout extends React.Component {
   }
 }
 
+const BreadcrumbWrapper = styled('div')`
+  display: flex;
+`;
+
 const NavMask = styled('div')`
+  display: none;
+
   @media (max-width: ${p => p.theme.breakpoints[0]}) {
     display: ${p => (p.isVisible ? 'block' : 'none')};
     background: rgba(0, 0, 0, 0.35);
@@ -91,16 +99,13 @@ const NavMask = styled('div')`
   }
 `;
 
-const StyledBreadcrumbWrapper = styled('div')`
-  display: flex;
-`;
-
-const SettingsHamburgerIcon = styled(Button)`
+const NavMenuToggle = styled(Button)`
+  display: none;
   margin-right: ${space(2)};
   color: ${p => p.theme.gray5};
-  display: none;
+
   @media (max-width: ${p => p.theme.breakpoints[0]}) {
-    display: ${p => (p.isVisible ? 'none' : 'block')};
+    display: block;
   }
 `;
 
@@ -112,9 +117,10 @@ const MaxWidthContainer = styled('div')`
 
 const SidebarWrapper = styled('div')`
   width: ${p => p.theme.settings.sidebarWidth};
-  background: #fff;
-  border-right: 1px solid ${p => p.theme.borderLight};
   padding: ${space(4)};
+  border-right: 1px solid ${p => p.theme.borderLight};
+  background: #fff;
+
   @media (max-width: ${p => p.theme.breakpoints[0]}) {
     animation: ${slideInLeft} 100ms ease-in-out;
     display: ${p => (p.isVisible ? 'block' : 'none')};
@@ -122,6 +128,7 @@ const SidebarWrapper = styled('div')`
     z-index: 3;
     height: 100%;
     box-shadow: 0 6px 15px rgba(0, 0, 0, 0.15);
+    overflow-y: auto;
   }
 `;
 
@@ -136,8 +143,8 @@ const SettingsColumn = styled('div')`
 `;
 
 /**
- * Note: `overflow: hidden` will cause some buttons in `SettingsPageHeader` to be cut off because it has negative margin.
- * Will also cut off tooltips.
+ * Note: `overflow: hidden` will cause some buttons in `SettingsPageHeader` to
+ * be cut off because it has negative margin.  Will also cut off tooltips.
  */
 
 const Content = styled('div')`
